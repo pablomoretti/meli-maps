1ffdb176b61516e33775d82cc9f23bd4d28b01bf
Pablo Moretti <pablomoretti@gmail.com>
2015-04-27 00:33:52 -0300

    remove index code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meli-maps
 Meli Maps
+
+https://gist.github.com/pablomoretti/74eaa914370b42718442
--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 var meli = require('./meli.js');
 var Promise = require('promise');
 
-meli.search("MLA","fernet").then(function(items){
-	items.forEach(function(item){
-		 meli.getCity(item.address.city_id).then(function(data){
-			var location = data.geo_information.location;
-			console.log(location.longitude + "," + location.latitude);
-		});
-	});
-});
 
 
 
